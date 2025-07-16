1d4912060bdcb33e1bea8e9688f51a07279193a4
yurkohudyma <hudyma@gmail.com>
2025-07-16 10:35:14 +0300

    init

---
+++ src/main/java/ua/hudyma/Wizzair2025Application.java
@@ -0,0 +1,13 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wizzair2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wizzair2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/hudyma/Wizzair2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.hudyma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wizzair2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
